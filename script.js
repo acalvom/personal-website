3f9754caf6a44b9a5963bb676bb2198d5c6ed007
acalvom <andreac.3108@gmail.com>
2022-02-23 11:06:19 +0100

    Play videos based on screen device and Internet connection

--- script.js
+++ script.js
@@ -33,4 +33,44 @@ $(document).ready(function () {
         var bootstrapToast = new bootstrap.Toast($('#toastContactForm'));
         bootstrapToast.show();
     });
+
+
+    const videosBackground = $(".video-background-holder video");
+    const screenWidth = screen.width;
+    const SCREEN_MEDIUM = 768, MBPS_MIN = 20.4;
+
+    [...videosBackground].forEach((video) => {
+        video.autoplay = false;
+        video.preload = "none"
+    });
+
+    setTimeout(() => playVideos(), 100); // Calculate Internet speed after 100ms
+
+    function playVideos() {
+        const downloadImage = "https://via.placeholder.com/1280x720.jpg";
+        let init = new Date().getTime();
+        let end;
+        let imageWeight = 114490 * 8; // Image weight in bits
+        calculateInternetSpeed((speedMbps) => {
+            if (screenWidth >= SCREEN_MEDIUM && speedMbps >= MBPS_MIN) {
+                [...videosBackground].forEach((video) => {
+                    video.autoplay = true;
+                    video.preload = "metadata";
+                    video.load();
+                });
+            }
+        });
+
+        function calculateInternetSpeed(callback) {
+            let imageObject = new Image();
+            imageObject.src = downloadImage;
+            imageObject.onload = function () {
+                end = new Date().getTime();
+                let downloadDuration = (end - init) / 1000;
+                let speedKbps = (imageWeight / downloadDuration / 1024).toFixed(2);
+                let speedMbps = (speedKbps / 1024).toFixed(2);
+                callback(speedMbps);
+            };
+        }
+    }
 });
